ee5f867ba181dd22733401eab01a2b27ff0cbd49
pradhapbarani <baranipradhap@gmail.com>
2026-04-13 09:41:14 +0300

    Update ToastBuildable with new properties and author

--- PowerplayToastKit/Classes/ToastBuildable.swift
+++ PowerplayToastKit/Classes/ToastBuildable.swift
@@ -2,7 +2,7 @@
 //  ToastBuildable.swift
 //  PowerplayToastKit
 //
-//  Created by Mithilesh Parmar on 18/10/21.
+//  Created by Barani Elangovan on 13/04/26.
 //
 
 import UIKit
@@ -14,13 +14,18 @@ public protocol ToastBuildable {
     var type: ToastType? { get }
     var timeInterval: TimeInterval? { get }
     var toastPosition: ToastPosition? { get }
-    
-    
+    var trailingIcon: UIImage? { get }
+    var viewHeight: Int? { get }
+    var showShadow: Bool? { get }
     @discardableResult func set(leadingIcon: UIImage?) -> ToastBuildable
+    @discardableResult func set(trailingIcon: UIImage?) -> ToastBuildable
     @discardableResult func set(type: ToastType?) -> ToastBuildable
     @discardableResult func set(theme: ToastThemeBuildable?) -> ToastBuildable
     @discardableResult func set(timeInterval: TimeInterval?) -> ToastBuildable
     @discardableResult func set(position: ToastPosition?) -> ToastBuildable
+    @discardableResult func set(viewHeight: Int?) -> ToastBuildable
+    @discardableResult func set(showShadow: Bool?) -> ToastBuildable
     @discardableResult func build() -> ToastBuildable
 }
+
 
